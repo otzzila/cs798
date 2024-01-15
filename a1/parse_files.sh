4a30cac3301e9356a94a4a4d20834a2ddf52193e
Owen Zila <otz.zila@gmail.com>
2024-01-14 22:40:03 -0500

    Fixed parse_files outputing to .txt not .csv

--- a1/parse_files.sh
+++ a1/parse_files.sh
@@ -2,9 +2,9 @@
 
 script_dir=$(dirname $0)
 
-echo -n "" >> results.txt
+echo -n "" >> results.csv
 
 for file in step*.txt
 do
-    $script_dir/parse_line.sh $file >> results.txt
+    $script_dir/parse_line.sh $file >> results.csv
 done
