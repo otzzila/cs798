80bc5d401e005e4d298cbfef8732c4bffd8f6188
Owen Zila <otz.zila@gmail.com>
2024-04-10 18:13:00 -0400

    hashtable initial

--- a7/hash/hashtable.h
+++ a7/hash/hashtable.h
@@ -26,6 +26,7 @@ private:
     bool isExpandNeeded(const int tid, int64_t probeCount);
     void expand(const int tid);
     int64_t getAccurateSize();
+    void migrateInsert(const int & key);
 
 public:
     TLEHashTableExpand(const int _numThreads, const int64_t _capacity);
@@ -71,24 +72,117 @@ void TLEHashTableExpand::expand(const int tid) {
     int64_t expansionStartTime = debugTimer.getElapsedMillis();
 
     // EXPANSION CODE HERE :)
+    int64_t accurateSize = getAccurateSize();
+
+    delete[] old;
+    old = data;
+
+    oldCapacity = capacity;
+    capacity = accurateSize * 4;
+
+    data = new volatile int [capacity];
+    for (int64_t i=0;i<capacity;++i) data[i] = EMPTY;
+
+    // Now move over old values
+    for (int64_t i=0;i<oldCapacity;++i){
+        int key = old[i];
+        if (key != EMPTY && key != TOMBSTONE){
+            migrateInsert(key);
+        }
+    }
+
+
 
 
 
     // suggested adjustment to counters at the end of expansion:
-    //  approxInserts->set(accurateSize);
-    //  approxDeletes->set(0);
+    approxInserts->set(accurateSize);
+    approxDeletes->set(0);
 
     auto expansionEndTime = debugTimer.getElapsedMillis();
     printf("tid=%d expansion at_ms=%ld duration_ms=%ld oldCapacity=%ld newCapacity=%ld\n", tid, expansionStartTime, (expansionEndTime - expansionStartTime), oldCapacity, capacity);
 }
 
+void TLEHashTableExpand::migrateInsert(const int & key){
+    int32_t h = murmur3(key);
+
+    for(int probe=0; probe < capacity; ++probe){
+        int index = (h+probe) % capacity;
+        int found = data[index];
+
+        if (found == EMPTY){
+            data[index] = key;
+            return;
+        }
+    }
+
+    assert(false);
+}
+
 // semantics: try to insert key. return true if successful (if key doesn't already exist), and false otherwise
 bool TLEHashTableExpand::insertIfAbsent(const int tid, const int & key) {
+    int32_t h = murmur3(key);
+restart:
+{
+    TLEGuard guard = TLEGuard(tid); // Must keep the guard out here in case capacity changes
+    for (int probeCount = 0; probeCount < capacity; ++probeCount){
+        
+        if (isExpandNeeded(tid, probeCount)){
+            guard.explicit_fallback();
+            // Not sure if this is required, but should be fast if we have the lock
+            if (isExpandNeeded(tid, probeCount)){
+                expand(tid);
+                goto restart;
+            }
+            
+        }
 
+        // Look at next value
+        int index = (h+probeCount) % capacity;
+        int found = data[index];
+
+        // Attempt the insert
+        if (found == key) {
+            guard.explicit_commit();
+            return false;
+        } else if (found == EMPTY){
+            data[index] = key;
+            approxInserts->inc(tid);
+            return true;
+        }
+        // else continue to next probeCount
+    }
+}
+
+
+    assert(false);
+    return false;
 }
 
 // semantics: try to erase key. return true if successful, and false otherwise
 bool TLEHashTableExpand::erase(const int tid, const int & key) {
+    int32_t h = murmur3(key);
+
+    {
+        TLEGuard guard = TLEGuard(tid);
+        for(int i=0; i < capacity; ++i){
+            int index = (h+i) % capacity;
+            int found = data[index];
+
+            if (found == key){
+                data[index] = TOMBSTONE;
+                approxDeletes->inc(tid);
+                return true;
+            } else if (found == EMPTY){
+                guard.explicit_commit();
+                return false;
+            }
+            // else continue to next value
+        }
+    }
+
+    assert(false);
+    return false;
 
 }
 
