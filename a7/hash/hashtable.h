ad241ec0aa95aab2d791b6f29bf04daf8e8f8451
Owen Zila <otz.zila@gmail.com>
2024-04-10 18:46:24 -0400

    allow shrink

--- a7/hash/hashtable.h
+++ a7/hash/hashtable.h
@@ -80,7 +80,7 @@ void TLEHashTableExpand::expand(const int tid) {
     TRACE {cout << "Expanding" << endl;}
     TRACE {PRINT(capacity); }
     oldCapacity = capacity;
-    capacity = max(max(accurateSize, int64_t(1)) * 4, oldCapacity);
+    capacity = max(accurateSize, int64_t(1)) * 8;
     TRACE {PRINT(capacity); }
 
     data = new volatile int [capacity];
